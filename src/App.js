a8006efcd9b899d1e5b057f9accb662031c12644
Ibsiany <ibsianyd@gmail.com>
2021-03-04 04:09:05 -0300

    project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="body">
+        <h1>Portfólio: CStéfany | Planeta TÉrra</h1>
+        <div className="link">
+          <a href="https://www.instagram.com/planetate.rra/" target="_blank">
+            <img src="./icons/instagram-logo.png" alt="Instagram" />
+          </a>
+          <a href="https://www.behance.net/cibellestefany" target="_blank">
+            <img src="./icons/behance-logo.png" alt="Behance" />
+          </a>
+          <a href="mailto:planetacsgc@gmail.com" target="_blank">
+            <img src="./icons/gmail-logo.png" alt="E-mail" />
+          </a>
+        </div>
+        <div className="list">
+          <a href="https://www.instagram.com/p/CLdN4efnuYp/?utm_source=ig_web_copy_link" target="_blank">
+            AUTORRETRATO
+            <img src="./images/meet.jpg" alt="Imagem Eu" />
+          </a>
+          <a href="https://www.instagram.com/p/CLIaSvpnPyS/?utm_source=ig_web_copy_link" target="_blank">
+            ANIVERSÁRIO
+            <img src="./images/aniversario.jpg" alt="Imagem Aniversário" />
+          </a>
+          <a href="https://www.instagram.com/p/CG0zRwMnTZE/?utm_source=ig_web_copy_link" target="_blank">
+            CASINHA DE COGUMELO
+           <img src="./images/cogumelo.jpg" alt="Imagem cogumelo" />
+          </a>
+          <a href="https://www.instagram.com/p/CGFfwYEH2QJ/?utm_source=ig_web_copy_link" target="_blank">
+            SEREIA NO DESERTO
+            <img src="./images/sereia.jpg" alt="Imagem sereia" />
+          </a>
+          <a href="https://www.instagram.com/p/CDbtaB-ng-e/?utm_source=ig_web_copy_link" target="_blank">
+            AUTORRETRATO
+            <img src="./images/eu.jpg" alt="Imagem eu" />
+          </a>
+          <a href="https://www.instagram.com/p/CFp8eTeHYSA/?utm_source=ig_web_copy_link" target="_blank">
+            VAN GOGH
+           <img src="./images/van-gogh.jpg" alt="Imagem Van Gogh" />
+          </a>
+          <a href="https://www.instagram.com/planetate.rra/" target="_blank">
+            ENCOMENDA - MARINA
+            <img src="./images/mari.jpg" alt="Imagem Mari - Encomenda" />
+          </a>
+          <a href="https://www.instagram.com/planetate.rra/" target="_blank">
+            ENCOMENDA - OTÁVIO
+            <img src="./images/otavio.jpg" alt="Imagem Otávio - Encomenda" />
+          </a>
+        </div>
+        <div className="link">
+          <a  id="image-first" href="https://www.instagram.com/planetate.rra/" target="_blank">
+            <img src="./icons/instagram-logo.png" alt="Instagram" />
+          </a>
+          <a id="image" href="https://www.behance.net/cibellestefany" target="_blank">
+            <img src="./icons/behance-logo.png" alt="Behance" />
+          </a>
+          <a id="image-last" href="mailto:planetacsgc@gmail.com" target="_blank">
+            <img src="./icons/gmail-logo.png" alt="E-mail" />
+          </a>
+          <p> | CIBELLE STÉFANY GOMES COSTA</p>
+        </div>
+      </div>
     </div>
   );
 }
