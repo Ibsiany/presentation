5bd5bcd933b70953d8f9502818d9670bdf10b553
ibsiany <ibsianyd@gmail.com>
2021-06-16 00:45:27 -0300

    ajustes imagens e links

--- src/App.js
+++ src/App.js
@@ -30,16 +30,16 @@ function App() {
            <img src="./images/cogumelo.jpg" alt="Imagem cogumelo" />
           </a>
           <a href="https://www.instagram.com/p/CGFfwYEH2QJ/?utm_source=ig_web_copy_link" target="_blank">
-            SEREIA NO DESERTO
-            <img src="./images/sereia.jpg" alt="Imagem sereia" />
+            ENCOMENDA DIA DOS NAMORADOS
+            <img src="./images/laura.jpeg" alt="Imagem sereia" />
           </a>
           <a href="https://www.instagram.com/p/CDbtaB-ng-e/?utm_source=ig_web_copy_link" target="_blank">
             AUTORRETRATO
             <img src="./images/eu.jpg" alt="Imagem eu" />
           </a>
           <a href="https://www.instagram.com/p/CFp8eTeHYSA/?utm_source=ig_web_copy_link" target="_blank">
-            VAN GOGH
-           <img src="./images/van-gogh.jpg" alt="Imagem Van Gogh" />
+            ENCOMENDA ARTE CANECA
+           <img src="./images/kitty.jpeg" alt="Imagem Van Gogh" />
           </a>
           <a href="https://www.instagram.com/planetate.rra/" target="_blank">
             ENCOMENDA - MARINA
@@ -48,6 +48,26 @@ function App() {
           <a href="https://www.instagram.com/planetate.rra/" target="_blank">
             ENCOMENDA - OTÁVIO
             <img src="./images/otavio.jpg" alt="Imagem Otávio - Encomenda" />
+          </a>
+          <a href="https://www.instagram.com/p/CNXmAltH1Xu/?utm_medium=copy_link" target="_blank">
+            DESIGN DE PERSONAGEM
+            <img src="./images/perfil.jpeg" alt="Imagem Mari - Encomenda" />
+          </a>
+          <a href="https://www.instagram.com/p/CNQJbn6nzhl/?utm_medium=copy_link" target="_blank">
+            ILUSTRAÇÃO FADAS
+            <img src="./images/fadas.jpeg" alt="Imagem Otávio - Encomenda" />
+          </a>
+          <a href="https://www.instagram.com/p/CNGFrycHf-d/?utm_medium=copy_link" target="_blank">
+            ILUSTRAÇÃO MENINA BOLHA
+            <img src="./images/bolha.jpeg" alt="Imagem Otávio - Encomenda" />
+          </a>
+          <a href="https://www.instagram.com/p/CMPewXlndYG/?utm_medium=copy_link" target="_blank">
+            ILUSTRAÇÃO AVATAR
+            <img src="./images/avatar.jpeg" alt="Imagem Mari - Encomenda" />
+          </a>
+          <a  href="https://www.instagram.com/p/CPo7dceHN6J/?utm_medium=copy_link" target="_blank">
+            ENCOMENDA BANNER
+            <img id="banner" src="./images/amor.jpeg" alt="Imagem Mari - Encomenda" />
           </a>
         </div>
         <div className="link">
